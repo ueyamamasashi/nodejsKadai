a0085ca82cdd3fc47e528c89359eb5d4f7f4f814
上山雅士 <ueyamamasashi@ueyamamasashi.local>
2021-02-06 19:30:44 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Laravel', subject: 'Login' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Laravel', subject: 'Register' });
+});
+
+module.exports = router;
